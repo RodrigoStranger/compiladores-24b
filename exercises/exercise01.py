cb9ba2aa65a162dbdde28364fd36774a016296c8
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2024-08-23 17:22:58 -0500

    update

--- exercises/exercise01.py
+++ exercises/exercise01.py
@@ -3,22 +3,17 @@
 vector = [1, 2, 3, 4, 5]
 
 # cola: el primero que se agrega es el primero en salir
-def enque(vector, valor):
-    vector.append(valor)
+def enque(vector, valor): vector.append(valor)
 
-def deque(vector):
-    del vector[0]
+def deque(vector): del vector[0]
 
 
 # pila: el último elemento que se agrega es el primero en salir
-def push(vector, valor):
-    vector.append(valor)
+def push(vector, valor): vector.append(valor)
 
-def pop(vector):
-    del vector[-1]
-
+def pop(vector): del vector[-1]
 
 
 print(vector)
-deque()
+deque(vector)
 print(vector)
