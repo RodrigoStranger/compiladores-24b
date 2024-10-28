e4452d6fc5fe3a36034ca42419ab5547b7245b25
Rodrigo Emerson Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2024-10-28 18:31:25 -0500

    update

---
+++ netcode/semantic.py
@@ -0,0 +1,8 @@
+from sintactic import parse_tree_root, success
+
+if success:
+    print("semantico xd")
+else: 
+    print(" ")
+    print("El análisis sintáctico ha fallado.")
+    
--- netcode/sintactic.py
+++ netcode/sintactic.py
@@ -1,6 +1,5 @@
-from lexic import listtokens
-#from functions_lexic import Token
 import os
+from lexic import listtokens
 from functions_sintactic import generate_table_ll1
 from functions_sintactic import arbolSintactico
 from functions_sintactic import arbolSintacticoContorno
@@ -12,30 +11,19 @@ pathfile2 = os.path.join(directory2, '..', 'table_ll1', sketchfile)
 
 table_ll1 = generate_table_ll1(pathfile2)
 
-#listtokens = []
-'''
-listtokens.append(Token("int", "int", None, None))
-listtokens.append(Token("+", "+", None, None))
-listtokens.append(Token("int", "int", None, None))
-listtokens.append(Token("$", "$", None, None))
-'''
-'''
-listtokens.append(Token("(", "(", None, None))
-listtokens.append(Token("int", "int", None, None))
-listtokens.append(Token(")", ")", None, None))
-listtokens.append(Token("$", "$", None, None))
-'''
 simboloinicial = "NETCODE"
 
 success, parse_tree_root = parser_sintactico_ll1(listtokens, table_ll1, simboloinicial)
 
+'''
 if success:
     output_folder = 'tree'
     if not os.path.exists(output_folder): os.makedirs(output_folder)
     graph = arbolSintacticoContorno(parse_tree_root)
     #graph = arbolSintactico(parse_tree_root)
-    output_pdf_path = os.path.join(output_folder, 'tree_sintactic_hola_mundo') 
+    output_pdf_path = os.path.join(output_folder, 'tree_sintactic_func_semantic_var') 
     graph.render(output_pdf_path, format='png', view=True)
 else:
     print(" ")
     print("El análisis sintáctico ha fallado.")
+'''
